f574223c5a1136d75574a7d6036c378c8e91ac0f
Saranya <sjsaranyasara@gmail.com>
2025-11-11 22:49:02 +0530

    initial commit

---
+++ lookup/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
